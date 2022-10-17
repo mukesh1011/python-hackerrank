90da07354c70b6fe5fc4a28984fd5c0c02559e39
mukesh1011 <37369551+mukesh1011@users.noreply.github.com>
2022-10-17 16:56:29 +0100

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n < 1 or n > 100:
+        print("N should be between 1 and 100")
+        exit()
+    if n % 2 == 0:
+        if n >= 2 and n <= 5:
+            print("Not Weird")
+        elif n >= 6 and n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+    else:
+        print("Weird")
